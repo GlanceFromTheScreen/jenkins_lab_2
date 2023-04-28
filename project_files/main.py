fa49fb720bbd2a73c3e6c498e7b97ac90ae20bc6
Evgeniy Hlamkin <98908654+GlanceFromTheScreen@users.noreply.github.com>
2023-04-28 14:48:52 +0300

    second

--- project_files/main.py
+++ project_files/main.py
@@ -1,3 +1,27 @@
-print('cat')
+from aiogram import types, executor, Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from config import TOKEN_API
+
+
+storage = MemoryStorage()
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot,
+                storage=storage)
+
+#  обработчик первой команды start
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message) -> None:
+    await message.answer('To-Do List Application!')
+
+
+@dp.message_handler(content_types=['text'])
+async def echo_(message: types.Message) -> None:
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp,
+                           skip_updates=True)
+
 
 
